4f1a19452265f7f6b78a1ddd77175eda7be0d463
fordsaranpong <forddbkzero@gmail.com>
2021-01-22 17:36:39 +0700

    Lab13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+T temp;
+int i=1;
+do{
+int x=i;	
+do{
+	
+if(d[x]>d[x-1]){temp=d[x];d[x]=d[x-1];d[x-1]=temp;} 
+	
+x--;
+}while(x>0);cout<<"Pass "<<i<<":";
+for(int j=0;j<N;j++){
+	cout<<d[j]<<" ";
+	
+}
+	cout<<"\n";	
+		
+i++;
+	
+}while(i<N);
 }
 
 int main(){
